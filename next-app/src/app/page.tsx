41ec53fb0cfff192f5bd43140bb66b55f13ef141
simeiro <shitimi1633@gmail.com>
2023-11-04 04:58:08 +0900

    デプロイ検証3

--- next-app/src/app/page.tsx
+++ next-app/src/app/page.tsx
@@ -7,12 +7,44 @@ import Event from "@/components/home/event/event";
 import Activity from '@/components/home/activity/activity';
 import Footer from "@/components/footer/footer";
 
-const Page = () => {
+
+async function getData() {
+  const displayMedias = [];
+    try{
+        const data = await fetch('https://graph.facebook.com/v18.0/17841452314795723?access_token=EABkbTKMqk5IBOZBZAUzJMizQbjaXl7NCl9sV0yRm0UwyRbxudPY6um3dUJMna1YDdnD0PrMYoaRHDTOpCRjmOxPRMaJ2zk6XhQ7FqAgRaOceNsJ7MG0IUC6Sy8y0IVh3yIpPrSq7tAAKOdTg3YYYPUo8abau7bZASZCjjw53XtUU1IvZBVCxsJZCJ1EJOX3Ngf8u4WM7IZD&fields=media%7Bmedia_url,thumbnail_url,permalink%7D');
+        const jsonData = await data.json();
+        for(let i = 0; i < 1; i++){
+            for(let i = 0; i < 25; i++){
+                if (jsonData["media"]["data"][i]["thumbnail_url"]){
+                    displayMedias.push([
+                        jsonData["media"]["data"][i]["media_url"], 
+                        jsonData["media"]["data"][i]["permalink"],
+                        jsonData["media"]["data"][i]["thumbnail_url"],
+                    ]);
+                }else{
+                    displayMedias.push([
+                        jsonData["media"]["data"][i]["media_url"],
+                        jsonData["media"]["data"][i]["permalink"],
+                    ]);
+                }
+            }
+        }
+  
+    }catch(e){
+        console.log(e);
+    }
+
+    return displayMedias;
+
+}
+
+export default async function Page() {
+  const data = await getData()
   return (
     <>
     <div className="relative  flex h-screen pt-10">
       <div className="absolute z-10 inset-0 flex justify-center pt-20">
-        <Product displayMedias={[]}></Product>
+        <Product displayMedias={data}></Product>
       </div>
       <div className="relative z-20 ">
         <div id="about" className='pt-2'>
@@ -41,4 +73,4 @@ const Page = () => {
   )
 };
 
-export default Page;
+
--- next-app/src/components/home/product.tsx
+++ next-app/src/components/home/product.tsx
@@ -1,52 +1,80 @@
 /* eslint-disable react/jsx-key */
 import ImageOrVideo from "@/components/home/imageOrVideo";
-import { InferGetServerSidePropsType, GetServerSideProps } from "next";
+// import {GetServerSideProps } from "next";
 type Props = {
     displayMedias: any[][];
 }
 
-export const getServerSideProps: GetServerSideProps<Props>= (async (context) => {
+// // const media = [];
 
-    const displayMedias = [];
-    try{
-        const data = await fetch('https://graph.facebook.com/v18.0/17841452314795723?access_token=EABkbTKMqk5IBOZBZAUzJMizQbjaXl7NCl9sV0yRm0UwyRbxudPY6um3dUJMna1YDdnD0PrMYoaRHDTOpCRjmOxPRMaJ2zk6XhQ7FqAgRaOceNsJ7MG0IUC6Sy8y0IVh3yIpPrSq7tAAKOdTg3YYYPUo8abau7bZASZCjjw53XtUU1IvZBVCxsJZCJ1EJOX3Ngf8u4WM7IZD&fields=media%7Bmedia_url,thumbnail_url,permalink%7D');
-        const jsonData = await data.json();
-        for(let i = 0; i < 1; i++){
-            for(let i = 1; i < 25; i++){
-                if (jsonData["media"]["data"][i]["thumbnail_url"]){
-                    displayMedias.push([
-                        jsonData["media"]["data"][i]["media_url"], 
-                        jsonData["media"]["data"][i]["permalink"],
-                        jsonData["media"]["data"][i]["thumbnail_url"],
-                    ]);
-                }else{
-                    displayMedias.push([
-                        jsonData["media"]["data"][i]["media_url"],
-                        jsonData["media"]["data"][i]["permalink"],
-                    ]);
-                }
-            }
-        }
-
-    }catch(e){
-        console.log(e);
-    }
-    console.log(displayMedias);
-    console.log("fdas")
-  
-    const props: Props = {
-        displayMedias: displayMedias,
-    };
-  
-    return {
-        props: props,
-    };
-})
-
-
-
-export default function Product(props: Props) {
+// export const getServerSideProps: GetServerSideProps<any> = (async (context) => {
     
+
+//     const displayMedias = [];
+//     try{
+//         const data = await fetch('https://graph.facebook.com/v18.0/17841452314795723?access_token=EABkbTKMqk5IBOZBZAUzJMizQbjaXl7NCl9sV0yRm0UwyRbxudPY6um3dUJMna1YDdnD0PrMYoaRHDTOpCRjmOxPRMaJ2zk6XhQ7FqAgRaOceNsJ7MG0IUC6Sy8y0IVh3yIpPrSq7tAAKOdTg3YYYPUo8abau7bZASZCjjw53XtUU1IvZBVCxsJZCJ1EJOX3Ngf8u4WM7IZD&fields=media%7Bmedia_url,thumbnail_url,permalink%7D');
+//         const jsonData = await data.json();
+//         for(let i = 0; i < 1; i++){
+//             for(let i = 1; i < 25; i++){
+//                 if (jsonData["media"]["data"][i]["thumbnail_url"]){
+//                     displayMedias.push([
+//                         jsonData["media"]["data"][i]["media_url"], 
+//                         jsonData["media"]["data"][i]["permalink"],
+//                         jsonData["media"]["data"][i]["thumbnail_url"],
+//                     ]);
+//                 }else{
+//                     displayMedias.push([
+//                         jsonData["media"]["data"][i]["media_url"],
+//                         jsonData["media"]["data"][i]["permalink"],
+//                     ]);
+//                 }
+//             }
+//         }
+  
+//     }catch(e){
+//         console.log(e);
+//     }
+//     console.log(displayMedias);
+//     console.log("fdas")
+  
+//     const props: Props = {
+//         displayMedias: displayMedias,
+//     };
+  
+//     return {
+//         props: props,
+//     };
+//     return media;
+//   })
+  
+
+
+
+export default async function Product(props: Props) {
+    // const displayMedias = [];
+    // try{
+    //     const data = await fetch('https://graph.facebook.com/v18.0/17841452314795723?access_token=EABkbTKMqk5IBOZBZAUzJMizQbjaXl7NCl9sV0yRm0UwyRbxudPY6um3dUJMna1YDdnD0PrMYoaRHDTOpCRjmOxPRMaJ2zk6XhQ7FqAgRaOceNsJ7MG0IUC6Sy8y0IVh3yIpPrSq7tAAKOdTg3YYYPUo8abau7bZASZCjjw53XtUU1IvZBVCxsJZCJ1EJOX3Ngf8u4WM7IZD&fields=media%7Bmedia_url,thumbnail_url,permalink%7D');
+    //     const jsonData = await data.json();
+    //     for(let i = 0; i < 1; i++){
+    //         for(let i = 0; i < 25; i++){
+    //             if (jsonData["media"]["data"][i]["thumbnail_url"]){
+    //                 displayMedias.push([
+    //                     jsonData["media"]["data"][i]["media_url"], 
+    //                     jsonData["media"]["data"][i]["permalink"],
+    //                     jsonData["media"]["data"][i]["thumbnail_url"],
+    //                 ]);
+    //             }else{
+    //                 displayMedias.push([
+    //                     jsonData["media"]["data"][i]["media_url"],
+    //                     jsonData["media"]["data"][i]["permalink"],
+    //                 ]);
+    //             }
+    //         }
+    //     }
+  
+    // }catch(e){
+    //     console.log(e);
+    // }
 
     return (
         <>
