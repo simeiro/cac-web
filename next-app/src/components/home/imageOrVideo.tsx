5a48fc5aac7e03369c3a8407d2354762b45dc6d4
simeiro <shitimi1633@gmail.com>
2023-11-04 05:05:46 +0900

    デプロイ検証img変化

--- next-app/src/components/home/imageOrVideo.tsx
+++ next-app/src/components/home/imageOrVideo.tsx
@@ -10,13 +10,14 @@ export default function ImageOrVideo(props: Props) {
         return(
             <div className="relative group bg-fixed">
                 <a href={props.url[1]} className="relative block overflow-hidden">
-                    <Image
+                    <img src={props.url[0]} alt="ImageOrVideos" className="px-4 py-10 opacity-10" width={480} height={270}/>
+                    {/* <Image
                     className="px-4 py-10 opacity-10"
                     src={props.url[0]}
                     alt="ImageOrVideos"
                     width={480}
                     height={270}
-                    />
+                    /> */}
                     {/* <div className="absolute inset-0 bg-gray-700 opacity-0 group-hover:opacity-50"></div>
                     <p className="absolute inset-0 flex items-center justify-center text-white opacity-0 group-hover:opacity-100">詳細を見る</p> */}
                 </a>
